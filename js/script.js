20d9fdec124d1134507ba9f5226d030f86be89da
gui.ljy <gui.ljy@zempublic.co.kr>
2023-06-09 16:30:37 +0900

    #불꽃놀이 효과
    [설명] 보일러플레이트 설정 방법1 - class 활용

--- js/script.js
+++ js/script.js
@@ -1,41 +1,45 @@
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d')
-const dpr = window.devicePixelRatio
-const fps = 60
-const interval = 1000 / fps
-let now, delta
-let then = Date.now()
+import CanvasOption from './CanvasOption.js'
 
-let canvasWidth, canvasHeight
+class Canvas extends CanvasOption {
+  constructor() {
+    super()
+  }
+  init(){
+    this.canvasWidth = innerWidth
+    this.canvasHeight = innerHeight
+    this.canvas.width = this.canvasWidth * this.dpr
+    this.canvas.height = this.canvasHeight * this.dpr
+    this.ctx.scale(this.dpr, this.dpr)
 
-function init(){
-  canvasWidth = innerWidth
-  canvasHeight = innerHeight
+    this.canvas.style.width = this.canvasWidth + 'px'
+    this.canvas.style.height = this.canvasHeight + 'px'
+  }
 
+  render(){
+    let now, delta
+    let then = Date.now()
 
-  canvas.width = canvasWidth * dpr
-  canvas.height = canvasHeight * dpr
-  ctx.scale(dpr, dpr)
+    const frame = () => {
+      requestAnimationFrame(frame)
+      now = Date.now()
+      delta = now - then
+      if (delta < this.interval) return
 
-  canvas.style.width = canvasWidth + 'px'
-  canvas.style.height = canvasHeight + 'px'
+      this.ctx.fillRect(100, 100, 200, 200)
+
+      then = now - (delta % this.interval)
+    }
+    requestAnimationFrame(frame)
+  }
 }
 
-function render(){
-  requestAnimationFrame(render)
-  now = Date.now()
-  delta = now - then
-  if (delta < interval) return
-
-  ctx. fillRect(100, 100, 200, 200)
-  then = now - (delta % interval)
-}
+const canvas = new Canvas()
 
 window.addEventListener('load',() => {
-  init()
-  render()
+  canvas.init()
+  canvas.render()
 })
 
 window.addEventListener('resize',() => {
-  init()
+  canvas.init()
 })
